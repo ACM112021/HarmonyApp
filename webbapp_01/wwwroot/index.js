660099f2b989b2070030c210bcf4101a646c0050
Andrew <github.537585@mailthink.net>
2023-06-10 12:31:12 -0500

    Removed Template

--- webbapp_01/wwwroot/index.js
+++ webbapp_01/wwwroot/index.js
@@ -1,22 +1,38 @@
 function webapp_02() {
 
-    //Get elements
+    // //Get elements
 
-    var textSearch = document.getElementById("text-search");
+    // var textSearch = document.getElementById("text-search");
 
-    var buttonSearch = document.getElementById("button-search");
-    var buttonSearchClear = document.getElementById("button-search-clear");
+    // var buttonSearch = document.getElementById("button-search");
+    // var buttonSearchClear = document.getElementById("button-search-clear");
 
-    var employeeTable = document.getElementById("employee-table");
+    // var employeeTable = document.getElementById("employee-table");
 
-    var buttonInsert = document.getElementById("button-insert");
-    var buttonInsertCancel = document.getElementById("button-insert-cancel");
+    // var buttonInsert = document.getElementById("button-insert");
+    // var buttonInsertCancel = document.getElementById("button-insert-cancel");
 
-    var buttonDelete = document.getElementById("button-delete");
-    var buttonDeleteCancel = document.getElementById("button-delete-cancel");
+    // var buttonDelete = document.getElementById("button-delete");
+    // var buttonDeleteCancel = document.getElementById("button-delete-cancel");
 
-    var buttonUpdate = document.getElementById("button-update");
-    var buttonUpdateCancel = document.getElementById("button-update-cancel");
+    // var buttonUpdate = document.getElementById("button-update");
+    // var buttonUpdateCancel = document.getElementById("button-update-cancel");
+
+
+    
+    // //Add event listeners
+
+    // buttonSearch.addEventListener("click", searchEmployees);
+    // buttonSearchClear.addEventListener("click", searchClear);
+
+    // buttonInsert.addEventListener("click", insertEmployee);
+    // buttonInsertCancel.addEventListener("click", insertEmployeeCancel);
+
+    // buttonDelete.addEventListener("click", deleteEmployee);
+    // buttonDeleteCancel.addEventListener("click", deleteEmployeeCancel);
+
+    // buttonUpdate.addEventListener("click", updateEmployee);
+    // buttonUpdateCancel.addEventListener("click", updateEmployeeCancel);
 
 
 
@@ -61,225 +77,212 @@ function webapp_02() {
    
 
 
-    //Add event listeners
 
-    buttonSearch.addEventListener("click", searchEmployees);
-    buttonSearchClear.addEventListener("click", searchClear);
 
-    buttonInsert.addEventListener("click", insertEmployee);
-    buttonInsertCancel.addEventListener("click", insertEmployeeCancel);
 
-    buttonDelete.addEventListener("click", deleteEmployee);
-    buttonDeleteCancel.addEventListener("click", deleteEmployeeCancel);
 
-    buttonUpdate.addEventListener("click", updateEmployee);
-    buttonUpdateCancel.addEventListener("click", updateEmployeeCancel);
 
 
 
 
+    // //Functions
 
+    // function searchEmployees() {
 
+    //     var url = 'http://localhost:5174/SearchEmployees?search=' + textSearch.value;
 
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.onreadystatechange = doAfterSearchEmployees;
+    //     xhr.open('GET', url);
+    //     xhr.send(null);
 
-    //Functions
+    //     function doAfterSearchEmployees() {
+    //         var DONE = 4; // readyState 4 means the request is done.
+    //         var OK = 200; // status 200 is a successful return.
+    //         if (xhr.readyState === DONE) {
+    //             if (xhr.status === OK) {
 
-    function searchEmployees() {
+    //                 var response = JSON.parse(xhr.responseText);
 
-        var url = 'http://localhost:5174/SearchEmployees?search=' + textSearch.value;
+    //                 if (response.result === "success") {
+    //                     showEmployees(response.employees);
+    //                 } else {
+    //                     alert("API Error: " + response.message);
+    //                 }
+    //             } else {
+    //                 alert("Server Error: " + xhr.status + " " + xhr.statusText);
+    //             }
+    //         }
+    //     };
 
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = doAfterSearchEmployees;
-        xhr.open('GET', url);
-        xhr.send(null);
+    // };
 
-        function doAfterSearchEmployees() {
-            var DONE = 4; // readyState 4 means the request is done.
-            var OK = 200; // status 200 is a successful return.
-            if (xhr.readyState === DONE) {
-                if (xhr.status === OK) {
+    // function showEmployees(employees) {
+    //     var employeeTableText = '<table class="table table-striped table-sm"><thead><tr><th scope="col">Empoyee ID</th><th scope="col">First Name</th><th scope="col">Last Name</th><th scope="col">Salary</th></tr></thead><tbody>';
 
-                    var response = JSON.parse(xhr.responseText);
+    //     for (var i = 0; i < employees.length; i++) {
+    //         var employee = employees[i];
 
-                    if (response.result === "success") {
-                        showEmployees(response.employees);
-                    } else {
-                        alert("API Error: " + response.message);
-                    }
-                } else {
-                    alert("Server Error: " + xhr.status + " " + xhr.statusText);
-                }
-            }
-        };
+    //         employeeTableText = employeeTableText + '<tr><th scope="row">' + employee.employeeId + '</th><td>' + employee.firstName + '</td><td>' + employee.lastName + '</td><td>' + employee.salary + '</td></tr>';
+    //     }
 
-    };
+    //     employeeTableText = employeeTableText + '</tbody></table>';
 
-    function showEmployees(employees) {
-        var employeeTableText = '<table class="table table-striped table-sm"><thead><tr><th scope="col">Empoyee ID</th><th scope="col">First Name</th><th scope="col">Last Name</th><th scope="col">Salary</th></tr></thead><tbody>';
+    //     employeeTable.innerHTML = employeeTableText;
+    // }
 
-        for (var i = 0; i < employees.length; i++) {
-            var employee = employees[i];
+    // function searchClear() {
+    //     textSearch.value = "";
+    //     searchEmployees();
+    // }
 
-            employeeTableText = employeeTableText + '<tr><th scope="row">' + employee.employeeId + '</th><td>' + employee.firstName + '</td><td>' + employee.lastName + '</td><td>' + employee.salary + '</td></tr>';
-        }
+    // function insertEmployee() {
 
-        employeeTableText = employeeTableText + '</tbody></table>';
+    //     var textFirstName = document.getElementById("text-insert-first-name");
+    //     var textLastName = document.getElementById("text-insert-last-name");
+    //     var textSalary = document.getElementById("text-insert-salary");
 
-        employeeTable.innerHTML = employeeTableText;
-    }
+    //     var url = 'http://localhost:5174/InsertEmployee?lastName=' + textLastName.value + '&firstName=' + textFirstName.value + '&salary=' + textSalary.value;
 
-    function searchClear() {
-        textSearch.value = "";
-        searchEmployees();
-    }
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.onreadystatechange = doAfterInsertEmployee;
+    //     xhr.open('GET', url);
+    //     xhr.send(null);
 
-    function insertEmployee() {
+    //     function doAfterInsertEmployee() {
+    //         var DONE = 4; // readyState 4 means the request is done.
+    //         var OK = 200; // status 200 is a successful return.
+    //         if (xhr.readyState === DONE) {
+    //             if (xhr.status === OK) {
 
-        var textFirstName = document.getElementById("text-insert-first-name");
-        var textLastName = document.getElementById("text-insert-last-name");
-        var textSalary = document.getElementById("text-insert-salary");
+    //                 var response = JSON.parse(xhr.responseText);
 
-        var url = 'http://localhost:5174/InsertEmployee?lastName=' + textLastName.value + '&firstName=' + textFirstName.value + '&salary=' + textSalary.value;
+    //                 if (response.result === "success") {
+    //                     showEmployees(response.employees);
+    //                 } else {
+    //                     alert("API Error: " + response.message);
+    //                 }
+    //             } else {
+    //                 alert("Server Error: " + xhr.status + " " + xhr.statusText);
+    //             }
+    //         }
+    //     };
 
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = doAfterInsertEmployee;
-        xhr.open('GET', url);
-        xhr.send(null);
+    //     textFirstName.value = "";
+    //     textLastName.value = "";
+    //     textSalary.value = "";
 
-        function doAfterInsertEmployee() {
-            var DONE = 4; // readyState 4 means the request is done.
-            var OK = 200; // status 200 is a successful return.
-            if (xhr.readyState === DONE) {
-                if (xhr.status === OK) {
+    // };
 
-                    var response = JSON.parse(xhr.responseText);
+    // function insertEmployeeCancel() {
 
-                    if (response.result === "success") {
-                        showEmployees(response.employees);
-                    } else {
-                        alert("API Error: " + response.message);
-                    }
-                } else {
-                    alert("Server Error: " + xhr.status + " " + xhr.statusText);
-                }
-            }
-        };
+    //     var textFirstName = document.getElementById("text-insert-first-name");
+    //     var textLastName = document.getElementById("text-insert-last-name");
+    //     var textSalary = document.getElementById("text-insert-salary");
 
-        textFirstName.value = "";
-        textLastName.value = "";
-        textSalary.value = "";
+    //     textFirstName.value = "";
+    //     textLastName.value = "";
+    //     textSalary.value = "";
 
-    };
+    // }
 
-    function insertEmployeeCancel() {
+    // //Update functions go here.
 
-        var textFirstName = document.getElementById("text-insert-first-name");
-        var textLastName = document.getElementById("text-insert-last-name");
-        var textSalary = document.getElementById("text-insert-salary");
+    // function updateEmployee() {
 
-        textFirstName.value = "";
-        textLastName.value = "";
-        textSalary.value = "";
+    //     var textEmployeeId = document.getElementById("text-update-employee-id");
+    //     var textFirstName = document.getElementById("text-update-first-name");
+    //     var textLastName = document.getElementById("text-update-last-name");
+    //     var textSalary = document.getElementById("text-update-salary");
 
-    }
+    //     var url = 'http://localhost:5174/UpdateEmployee?employeeid=' + textEmployeeId.value + '&lastName=' + textLastName.value + '&firstName=' + textFirstName.value + '&salary=' + textSalary.value;   //An excercise for the reader...
 
-    //Update functions go here.
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.onreadystatechange = doAfterUpdateEmployee;
+    //     xhr.open('GET', url);
+    //     xhr.send(null);
 
-    function updateEmployee() {
+    //     function doAfterUpdateEmployee() {
+    //         var DONE = 4; // readyState 4 means the request is done.
+    //         var OK = 200; // status 200 is a successful return.
+    //         if (xhr.readyState === DONE) {
+    //             if (xhr.status === OK) {
 
-        var textEmployeeId = document.getElementById("text-update-employee-id");
-        var textFirstName = document.getElementById("text-update-first-name");
-        var textLastName = document.getElementById("text-update-last-name");
-        var textSalary = document.getElementById("text-update-salary");
+    //                 var response = JSON.parse(xhr.responseText);
 
-        var url = 'http://localhost:5174/UpdateEmployee?employeeid=' + textEmployeeId.value + '&lastName=' + textLastName.value + '&firstName=' + textFirstName.value + '&salary=' + textSalary.value;   //An excercise for the reader...
+    //                 if (response.result === "success") {
+    //                     showEmployees(response.employees);
+    //                 } else {
+    //                     alert("API Error: " + response.message);
+    //                 }
+    //             } else {
+    //                 alert("Server Error: " + xhr.status + " " + xhr.statusText);
+    //             }
+    //         }
+    //     };
 
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = doAfterUpdateEmployee;
-        xhr.open('GET', url);
-        xhr.send(null);
+    //     textEmployeeId.value = "";
+    //     textFirstName.value = "";
+    //     textLastName.value = "";
+    //     textSalary.value = "";
 
-        function doAfterUpdateEmployee() {
-            var DONE = 4; // readyState 4 means the request is done.
-            var OK = 200; // status 200 is a successful return.
-            if (xhr.readyState === DONE) {
-                if (xhr.status === OK) {
+    // };
 
-                    var response = JSON.parse(xhr.responseText);
+    // function updateEmployeeCancel() {
 
-                    if (response.result === "success") {
-                        showEmployees(response.employees);
-                    } else {
-                        alert("API Error: " + response.message);
-                    }
-                } else {
-                    alert("Server Error: " + xhr.status + " " + xhr.statusText);
-                }
-            }
-        };
+    //     var textEmployeeId = document.getElementById("text-update-employee-id");
+    //     var textFirstName = document.getElementById("text-update-first-name");
+    //     var textLastName = document.getElementById("text-update-last-name");
+    //     var textSalary = document.getElementById("text-update-salary");
 
-        textEmployeeId.value = "";
-        textFirstName.value = "";
-        textLastName.value = "";
-        textSalary.value = "";
+    //     textEmployeeId.value = "";
+    //     textFirstName.value = "";
+    //     textLastName.value = "";
+    //     textSalary.value = "";
 
-    };
+    // }
 
-    function updateEmployeeCancel() {
 
-        var textEmployeeId = document.getElementById("text-update-employee-id");
-        var textFirstName = document.getElementById("text-update-first-name");
-        var textLastName = document.getElementById("text-update-last-name");
-        var textSalary = document.getElementById("text-update-salary");
+    // //Delete functions go here.
 
-        textEmployeeId.value = "";
-        textFirstName.value = "";
-        textLastName.value = "";
-        textSalary.value = "";
+    // function deleteEmployee() {
 
-    }
+    //     var textEmployeeId = document.getElementById("text-delete-employee-id");
 
+    //     var url = 'http://localhost:5174/DeleteEmployee?employeeid=' + textEmployeeId.value;
 
-    //Delete functions go here.
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.onreadystatechange = doAfterDeleteEmployee;
+    //     xhr.open('GET', url);
+    //     xhr.send(null);
 
-    function deleteEmployee() {
+    //     function doAfterDeleteEmployee() {
+    //         var DONE = 4; // readyState 4 means the request is done.
+    //         var OK = 200; // status 200 is a successful return.
+    //         if (xhr.readyState === DONE) {
+    //             if (xhr.status === OK) {
 
-        var textEmployeeId = document.getElementById("text-delete-employee-id");
+    //                 var response = JSON.parse(xhr.responseText);
 
-        var url = 'http://localhost:5174/DeleteEmployee?employeeid=' + textEmployeeId.value;
+    //                 if (response.result === "success") {
+    //                     showEmployees(response.employees);
+    //                 } else {
+    //                     alert("API Error: " + response.message);
+    //                 }
+    //             } else {
+    //                 alert("Server Error: " + xhr.status + " " + xhr.statusText);
+    //             }
+    //         }
+    //     };
 
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = doAfterDeleteEmployee;
-        xhr.open('GET', url);
-        xhr.send(null);
+    //     textEmployeeId.value = "";
 
-        function doAfterDeleteEmployee() {
-            var DONE = 4; // readyState 4 means the request is done.
-            var OK = 200; // status 200 is a successful return.
-            if (xhr.readyState === DONE) {
-                if (xhr.status === OK) {
+    // };
 
-                    var response = JSON.parse(xhr.responseText);
-
-                    if (response.result === "success") {
-                        showEmployees(response.employees);
-                    } else {
-                        alert("API Error: " + response.message);
-                    }
-                } else {
-                    alert("Server Error: " + xhr.status + " " + xhr.statusText);
-                }
-            }
-        };
-
-        textEmployeeId.value = "";
-
-    };
-
-    function deleteEmployeeCancel() {
-        var textEmployeeId = document.getElementById("text-delete-employee-id");
-        textEmployeeId.value = "";
-    }
+    // function deleteEmployeeCancel() {
+    //     var textEmployeeId = document.getElementById("text-delete-employee-id");
+    //     textEmployeeId.value = "";
+    // }
 
 
 
@@ -530,7 +533,7 @@ function webapp_02() {
 
 
     //Invoke searchEmployees() on load
-    searchEmployees();
+    // searchEmployees();
 
     searchMusicSheets();
 }
